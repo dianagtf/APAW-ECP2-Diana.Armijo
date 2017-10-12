ddabc585569e3cad4c27d59874b1ffe46f39133a
Diana <Diana@DianaPC>
2017-10-12 13:57:58 +0200

    Issue#02: new test for seeProfessional

---
+++ src/test/java/es/upm/miw/apaw/ecp2/api/controllers/ProfessionalControllerT.java
@@ -0,0 +1,37 @@
+package es.upm.miw.apaw.ecp2.api.controllers;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.upm.miw.apaw.ecp2.api.daos.DaoFactory;
+import es.upm.miw.apaw.ecp2.api.daos.memory.DaoMemoryFactory;
+import es.upm.miw.apaw.ecp2.api.entities.Professional;
+
+public class ProfessionalControllerT {
+	
+	@Before
+    public void before() {
+		long phone = 11111111;
+		
+		DaoFactory.setFactory(new DaoMemoryFactory());
+		
+		DaoFactory.getFactory().getProfessionalDao().create(new Professional(phone));
+
+    }
+	
+	@Test
+    public void testSeeProfessional() {
+		long phone2 = 55555555;
+		
+		DaoFactory.getFactory().getProfessionalDao().create(new Professional(phone2));
+		assertEquals(2, DaoFactory.getFactory().getProfessionalDao().findAll().size());
+		assertEquals(phone2, DaoFactory.getFactory().getProfessionalDao().findAll().get(1).getPhone());
+
+    }
+
+}
