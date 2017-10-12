5a9a69720614038b43817c601726c4ff9551cf5c
Diana <Diana@DianaPC>
2017-10-12 20:06:30 +0200

    Test for issue#03

--- src/test/java/es/upm/miw/apaw/ecp2/api/ProfessionalResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/ecp2/api/ProfessionalResourceFunctionalTesting.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import es.upm.miw.apaw.ecp2.api.daos.DaoFactory;
 import es.upm.miw.apaw.ecp2.api.daos.memory.DaoMemoryFactory;
+import es.upm.miw.apaw.ecp2.api.entities.Professional;
 import es.upm.miw.apaw.ecp2.api.resources.OrderResource;
 import es.upm.miw.apaw.ecp2.api.resources.ProfessionalResource;
 import es.upm.miw.apaw.ecp2.http.HttpClientService;
@@ -51,6 +52,17 @@ public class ProfessionalResourceFunctionalTesting {
 		this.createProfessional();
         HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(ProfessionalResource.PROFESSIONALS).path(ProfessionalResource.PHONES).expandPath("1").build();
         assertEquals("{\"id\":1,\"phones\":\"555555555\"}", new HttpClientService().httpRequest(request).getBody());
+    }
+	
+	@Test
+    public void testSeeLevelProfessional() {
+		long phone2 = 55555555;
+		
+		DaoFactory.getFactory().getProfessionalDao().create(new Professional(phone2));
+		
+		HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path(ProfessionalResource.PROFESSIONALS).path(ProfessionalResource.PHONES).path(ProfessionalResource.LEVEL).body("1:55555555:NORMAL").build();
+		
+		assertEquals("[{1:55555555:NORMAL}]", new HttpClientService().httpRequest(request).getBody());
 
     }
 	
